e3d679d1fc730f5399d15b29459e75ca5e236851
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-04-18 11:06:05 -0700

    insertion

---
+++ INSERTION query.sql
@@ -0,0 +1,7 @@
+INSERT INTO EMP3(CustomerID,CustomerName,ContactName,Adderess,City,PostalCode,Country)
+
+VALUES  (1,'Alfreds','Maria','Ober.57','Berlin',12209,'Germany'),
+		(2,'Ana','Ana tri','Ober.2222','Mexico D.F',05021,'Mexico'),
+		(3,'Antino','Antino','Ober2013','Mexico D.F',05023,'Mexico'),
+		(4,'Around n','Thomas','120r SQ.','LONDON',12209,'UK'),
+		(5,'Berglunds','Chirsitna','Bee 8','Lulea',95822,'Sweden');
