7c8e6de15cb3df809479be257f6cf8bd3b0e1fd9
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-04-18 11:06:26 -0700

    lab 02

---
+++ Lab 02- Database & Table Creation.sql
@@ -0,0 +1,25 @@
+-- step 01- how to create a database
+
+create database bscsfall2020
+
+--small letter
+--without space/underscore
+
+--step 02- How to use a database
+use bscsfall2020
+
+--step 03- How to create a table 
+--1. tablename (unique)
+--2. columnname (small alphabet/without space)
+--3. datatypes, size
+--18-arid-1
+create table students(stdid varchar(15) primary key,
+stdname varchar(25), gender char(06), marks int)
+--dbo stands for database objects
+-- 
+
+
+
+
+
+ 
