ff566a5d0944adfb9d744736104ef75f9a637291
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-04-18 11:05:56 -0700

    insert data in table

---
+++ Insert data into table2.sql
@@ -0,0 +1,4 @@
+INSERT INTO EMP2(Id,Name,RollNo,Section,Course,CourseCode,Campus)
+values(3,'Hamayun',019,'IT19A','Database','IT-243','Marghzar'),
+(4,'Tayyab',018,'IT19A','Database','IT-243','Marghzar');
+
